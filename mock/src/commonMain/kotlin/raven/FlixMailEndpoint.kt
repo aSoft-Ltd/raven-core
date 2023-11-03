efef681b66532d937eeef84e490c4f59322d6af7
andylamax <andylamax@programmer.net>
2023-11-04 02:21:37 +0300

    Published picapital and pimonitor

--- mock/src/commonMain/kotlin/raven/FlixMailEndpoint.kt
+++ mock/src/commonMain/kotlin/raven/FlixMailEndpoint.kt
@@ -1,5 +1,6 @@
 package raven
 
+@Deprecated("Do not use. Use raven.SanityEndpointInstead")
 class FlixMailEndpoint(private val base: String) {
     fun send() = "$base/mail/send"
     fun mailbox() = "$base/mail/box"
